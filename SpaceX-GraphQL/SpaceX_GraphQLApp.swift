752a4b8da4fe876cb58552db02e9237eb7e371aa
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2022-09-23 21:25:04 +0200

    Initial commit

---
+++ SpaceX-GraphQL/SpaceX_GraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_GraphQLApp.swift
+//  SpaceX-GraphQL
+//
+//  Created by Piotr Prokopowicz on 23/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_GraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
